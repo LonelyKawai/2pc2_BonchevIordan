05b5d8169182f3b647f977f79c87bdcabac2f755
LonelyKawai <bong334090@mail.ru>
2021-09-28 14:01:17 +0500

    test edit

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -6,7 +6,42 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("работа с git!");
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+
+            Console.WriteLine("первое задание");
+            for (int i = 0; i <= 60; i += 5)
+            {
+                Console.WriteLine();
+            }
+            Console.WriteLine("второе задание");
+            for (int j = 6; j < 5; j++) 
+            {
+                Console.WriteLine(letters [j]);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("третье задание");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("###");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("четвертое задание");
+            for (int j = 0; j <= 100; j++)
+            {
+                if (j % 5 == 0)
+                {
+                    Console.WriteLine(j + " ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("пятое задание");
+            for (int i = 2, j = 40; i - j != 20; i++, j--)
+            {
+                Console.WriteLine(i + " " + j);
+            }
         }
     }
 }
