f502ff6383f454a14ced863d7b91485956fbad78
LonelyKawai <bong334090@mail.ru>
2021-10-14 17:17:59 +0500

    TEST

--- pz_8/Program.cs
+++ pz_8/Program.cs
@@ -10,7 +10,53 @@ namespace pz_8
             int i;
             Random rnd = new Random();
             for (i = 0; i <= 11; i = i + 1)
-             Console.Write($"  {rnd.Next(-12, 12)} ");
+                A[i] = rnd.Next(-12, 12);
+            foreach (int j in A)
+            {
+                Console.Write(j + " ");
+            }
+            A = OneStepRight(A);
+            A = OneStepRight(A);
+            A = OneStepRight(A);
+            A = OneStepRight(A);
+            Console.WriteLine();
+            foreach (int j in A)
+            {
+                Console.Write(j + " ");
+            }
+
+
+        }
+        public static int[] OneStepRight(int[] Arr)
+        {
+
+            int temp2 = 0;
+            int temp3 = 0;
+            int temp1 = Arr[Arr.Length - 1];
+            for (int i = 0; i < Arr.Length - 1; i++)
+            {
+                if (i > 0)
+                {
+                    if (i % 2 != 0)
+                    {
+                        temp3 = Arr[i + 1];
+                        Arr[i + 1] = temp2;
+                    }
+                    else
+                    {
+                        temp2 = Arr[i + 1];
+                        Arr[i + 1] = temp3;
+                    }
+                }
+                else
+                {
+                    temp2 = Arr[1];
+                    Arr[i + 1] = Arr[i];
+                }
+
+            }
+            Arr[0] = temp1;
+            return Arr;
         }
     }
 }
