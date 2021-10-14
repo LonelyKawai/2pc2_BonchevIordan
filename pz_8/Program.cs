51ca1c720d371d4546a78739976cc889ec916bd4
LonelyKawai <bong334090@mail.ru>
2021-10-14 11:41:30 +0500

    TEST

---
+++ pz_8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int [12];
+            int i;
+            Random rnd = new Random();
+            for (i = 0; i <= 11; i = i + 1)
+             Console.Write($"  {rnd.Next(-12, 12)} ");
+
+            
+
+        }
+    }
+}
