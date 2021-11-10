058c60e2f0c1962db77f51ee9a84b06bce7a3e68
LonelyKawai <bong334090@mail.ru>
2021-11-11 03:56:09 +0500

    test

---
+++ pz_11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] charArray = str.ToCharArray();
+            int res = 0;
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (charArray[0] == '_' | Regex.IsMatch(charArray[0].ToString(), @"[a-zA-Z\d]"))
+                    {
+                        res += 1;
+                    }
+                }
+                else
+                {
+                    if (Regex.IsMatch(charArray[i].ToString(), @"[a-zA-Z\d]"))
+                    {
+                        res += 1;
+                    }
+                }
+            }
+
+            if (res == charArray.Length)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
