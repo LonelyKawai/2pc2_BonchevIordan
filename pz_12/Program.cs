af0f9b45f06b332a415d1bbf269d9d176d58b5bc
LonelyKawai <bong334090@mail.ru>
2021-11-12 06:15:12 +0500

    test

---
+++ pz_12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace pz_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] numbers = Regex.Split(input, @"\D+");           
+            foreach (string value in numbers)
+            {
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        int i = int.Parse(value);
+                        Console.Write(" {0}", i);
+                    }
+            }
+        }
+    }
+}
