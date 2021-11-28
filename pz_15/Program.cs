a56b6f60533576d4e8994d985a42778f41e9c8a1
LonelyKawai <bong334090@mail.ru>
2021-11-28 15:12:31 +0500

    test

---
+++ pz_15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace pz_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream(@"C:\Users\User\Desktop\f1.txt", FileMode.Open, FileMode.ReadWrite);
+            // подчеркивает красным а я не понимаю почему (@"C:\Users\User\Desktop\f1.txt") (FileMode.Open) (ReadWrite)
+            StreamReader reader = new StreamReader(file);
+            string[] str = reader.ReadToEnd().Split("\n");
+            reader.Close();
+            int a = 0;
+            for(int i = 0; i < file.Length; i++)
+            {
+                if(str[i].Length > a)
+                {
+                    a = i; 
+                }
+                Console.WriteLine(str[a]);
+            }
+        }
+    }
+}
