e6a7c5d000d1f90d67ad712cf83ac2b7e4b1f61e
LonelyKawai <bong334090@mail.ru>
2022-02-10 17:54:42 +0500

    успел)

---
+++ pz_3_create_class/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_3_create_class
+{
+    class Address
+    {
+        public string country;
+        public string city;
+        public string street;
+        public int house;
+        public int flat;
+        public Address(string a, string b, string c, int d, int e)
+        {
+            country = a;
+            city = b;
+            street = c;
+            house = d;
+            flat = e;
+        }
+        public void GetAddress()
+        {
+            Console.WriteLine($"country = {country}, city = {city}, street = {street}, house = {house}, flat = {flat}");
+        }
+
+
+    }
+
+}
+
---
+++ pz_3_create_class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz_3_create_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address house1 = new Address("Germany", "munhen", "agoi", 43, 2);
+            Address house2 = new Address("Usa", "london", "jdfhg", 44, 1);
+            Address house3 = new Address("Russia", "moscow", "gjkdfhkg", 45, 3);
+            Address house4 = new Address("Bulgaria", "plowdiv", "fgjhdjkg", 46, 5);
+            house1.GetAddress();
+            house2.GetAddress();
+            house3.GetAddress();
+            house4.GetAddress();
+        }
+    }
+}
